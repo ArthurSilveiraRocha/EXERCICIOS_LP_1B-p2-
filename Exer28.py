a8011340f074a4a5a6de59c503ed7f6eed49fa16
ArthurSilveiraRocha <88682674+ArthurSilveiraRocha@users.noreply.github.com>
2021-08-26 14:42:56 -0300

    Create Exer28.py

---
+++ Exer28.py
@@ -0,0 +1,5 @@
+x = float(input("Insira um valor: "))
+y = float(input("Insira outro valor: "))
+z = float(input("Insira outro valor: "))
+s = ((x**2)+(y**2)+(z**2))
+print ("A soma do quadrados dos números {}, {} e {} é {:.3f}".format(x, y, z, s))
